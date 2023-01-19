f72e0e9ac4aaa4a70fead4f0b7522ef499f91e9f
Chara White <chara@chara.pub>
2023-01-19 21:45:42 +0800

    fix xposed desc message

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "pub.chara.miuipadmeta"
         minSdk = 30
         targetSdk = 32
-        versionCode = 3
-        versionName = "2"
+        versionCode = 4
+        versionName = "2.1"
     }
 
     buildTypes {
